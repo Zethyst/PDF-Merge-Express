3f977ce8514a2609ccd8fbc3bc4269291cf59ae0
Ekansh <zethyst@protonmail.com>
2023-07-01 04:23:21 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const postWork=()=>{
+    // alert(document.querySelector("input"));
+    // console.log(document.querySelector("input"));
+    // alert(document.querySelector("#downloadBtn"))
+}
+module.exports = {postWork}

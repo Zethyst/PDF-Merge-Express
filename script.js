9b09854e8bf0b3cc6400a911f41784177fda6dc7
Ekansh <zethyst@protonmail.com>
2023-07-02 04:37:09 +0530

    Fixed some bugs

--- script.js
+++
@@ -1,6 +0,0 @@
-const postWork=()=>{
-    // alert(document.querySelector("input"));
-    // console.log(document.querySelector("input"));
-    // alert(document.querySelector("#downloadBtn"))
-}
-module.exports = {postWork}
